5032f1da590d25d1b000026903f5780925d62fcc
Antonio Zurano <azurano@outlook.es>
2025-03-07 22:54:15 +0100

    Añadir método mostrar_vehiculo y mejorar la lógica de frenado en la clase vehiculo

--- abstrac.py
+++ abstrac.py
@@ -5,6 +5,9 @@ class vehiculo:
         self._encendido = False
         self.velocidad = 0
 
+    def mostrar_vehiculo(self):
+        print(f"Marca: {self.marca}, Modelo: {self.modelo}")
+
     def encender(self):
             self._encendido = True
             print("El vehículo ha sido encendido")
@@ -22,7 +25,22 @@ class vehiculo:
 
     def frenar(self, velocidad):
         if self._encendido:
-            self.velocidad -= velocidad
-            print(f"La velocidad del vehículo es de {self.velocidad} km/h")
+            if self.velocidad - velocidad >= 0:
+                self.velocidad -= velocidad
+                print(f"La velocidad del vehículo es de {self.velocidad} km/h")
+            else:
+                print("La velocidad no puede ser negativa")
         else:
             print("El vehículo está apagado")
+
+
+vehiculo1 = vehiculo("Toyota", "Corolla") 
+vehiculo1.mostrar_vehiculo()
+vehiculo1.encender()
+vehiculo1.acelerar(50)
+vehiculo1.frenar(20)
+vehiculo1.apagar()
+vehiculo1.acelerar(50)
+vehiculo1.frenar(20)
+vehiculo1.acelerar(50)
+vehiculo1.encender()        
